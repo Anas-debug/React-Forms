e61f5b7d39114bf64475a8bb2a3a3181a5fda2b4
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-05 18:07:45 +0100

    Initialized a Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,11 @@
 import React from 'react'
+
+export default function Form(){
+    <from>
+        <input
+        type="text"
+        placeholder="First name"
+        >
+        </input>
+    </from>
+}
