cb1a3d149c0bd2d1d7a59181abb16ed57241d353
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-05 18:24:07 +0100

    handleChange function initialized

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,11 +4,16 @@ import { useState } from 'react';
 export default function Form(){
     const [firstName, setFirstName] = useState("");
 
+    function handleChange(){
+        console.log("Changed!");
+    }
+
     return(
     <from>
         <input
         type="text"
         placeholder="First name"
+        onChange={ handleChange }
         >
         </input>
     </from>);
