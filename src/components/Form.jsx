64d4769cd274d1bfc6bbe33e7c24a9f2eb0d4ab1
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-05 18:21:28 +0100

    Removed default App and replaced it's contents with the Form Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Form(){
+    const [firstName, setFirstName] = useState("");
+
+    return(
     <from>
         <input
         type="text"
         placeholder="First name"
         >
         </input>
-    </from>
+    </from>);
+
 }
